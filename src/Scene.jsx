af0b383686e31459d88c737b59029af2be09e14a
Vasilii Buraev <buraevvasilii@gmail.com>
2022-12-07 11:00:00 +0300

    add box Scene.jsx

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,4 +1,24 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useFrame } from "@react-three/fiber";
 
+function Box(props) {
+  const mesh = useRef()
 
+  return (
+    <mesh{...props}>
+      <boxGeometry args = {[1,1,1]} />
+      <meshStandardMaterial color={'orange'} />
+    </mesh>
+  )
+}
+
+export function Scene() {
+   return (
+     <>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position = {[-1.2, 0, 0]} />
+      <Box position = {[1.2, 0, 0]} />
+     </>
+   ) 
+} 
