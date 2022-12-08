00d6625ea943dba1caa30ede8222f0fd7e7a92de
Vasilii Buraev <buraevvasilii@gmail.com>
2022-12-08 11:00:00 +0300

    add color from box Scene.jsx

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -2,12 +2,23 @@ import { useRef, useState } from "react";
 import { useFrame } from "@react-three/fiber";
 
 function Box(props) {
-  const mesh = useRef()
+  const mesh = useRef();
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  useFrame((state, delta) => (mesh.current.rotation.x += delta))
 
   return (
-    <mesh{...props}>
+    <mesh 
+      {...props} 
+      ref={mesh} 
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+      
+    >
       <boxGeometry args = {[1,1,1]} />
-      <meshStandardMaterial color={'orange'} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
     </mesh>
   )
 }
