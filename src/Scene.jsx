a9dd400199cc8137cb7bb75cc133d0d0904ab64d
Vasilii Buraev <buraevvasilii@gmail.com>
2022-12-12 11:00:00 +0300

    add map shadow

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -11,6 +11,7 @@ function Box(props) {
     <mesh 
       {...props} 
       ref={mesh} 
+      castShadow
       scale={active ? 1.5 : 1}
       onClick={(event) => setActive(!active)}
       onPointerOver={(event) => setHover(true)}
@@ -27,7 +28,11 @@ export function Scene() {
    return (
      <>
       <ambientLight intensity={0.1} />
-      <pointLight position={[10, 10, 10]} castShadow />
+      <pointLight position={[10, 10, 10]} 
+       castShadow
+       shadow-mapSize-width={2048}
+       shadow-mapSize-height={2048}
+       />
       <Box position = {[-1.2, 0, 0]} />
       <Box position = {[1.2, 0, 0]} />
      </>
